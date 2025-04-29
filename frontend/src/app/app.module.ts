bc47ec6750a3132e9da36eab97f210fe0b60da46
CristianEguia <eguia.c@gmail.com>
2025-04-29 12:56:55 -0400

    Final commit: downgraded Angular version due to visualization errors with standalone structure

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from './app-routing.module'; 
 
 import { AppComponent } from './app.component';
 import { BookListComponent } from './components/book-list/book-list.component';
@@ -18,9 +18,9 @@ import { BookEditComponent } from './components/book-edit/book-edit.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    AppRoutingModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
